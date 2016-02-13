03aa1d35f4cfbd388dcf402290af8976ec123fdc
Sheri K Mizuha <SKM@MacBook-Air-2.local>
2016-02-12 21:05:38 -1000

    commpleted for loops exercise

--- work/loops.js
+++ work/loops.js
@@ -44,7 +44,7 @@ function createEvenNumberArray(count) {
 	var evenNumber;
 
 	evenNumberArray = [];
-	for (i = 0, i < count; i++) {
+	for (i = 0; i < count; i++) {
 		evenNumber = 2 * i;
 		evenNumberArray.push(evenNumber);
 
@@ -81,10 +81,10 @@ for (i=0; i < napSchedule.length; i++){
 
 function nap(schedule){
 	if (schedule){
-		console.log ("ZzZzZzZz")
+		//console.log ("ZzZzZzZz")
 	}
 	else {
-		console.log ("Gotta get to work!");
+		//console.log ("Gotta get to work!");
 		isNapTime = true; 
 	}
 }
@@ -95,19 +95,87 @@ valuesArray = [99, 66, 829, 1941, 8, 76];
 
 copyArray(valuesArray, copyOfValuesArray)
 
-console.log(valuesArray);
-console.log(copyOfValuesArray);
-function copiesArray (originArray, destinationArray){
+//console.log(valuesArray);
+//console.log(copyOfValuesArray);
+function copyArray (originArray, destinationArray){
 	for (i=0; i < originArray.length; i++){
 		destinationArray.push(originArray[i]);
 	}
 }
 
+var miscStorage;
+
+miscStorage = [ [], "Carrots", 9, "Beets", {}, {name: "Todd B."}, "Mush" ]
+//console.log (generateArrayOfStrings(miscStorage));
+
+function generateArrayOfStrings(storage){
+	var stringArray;
+
+	stringArray = [];
+	for (i = 0; i < storage.length; i++){
+		if (typeof storage[i] === "string"){
+			stringArray.push (storage[i]);
+		}
+	}
+	return stringArray;
+}
+var currentClass;
+currentClass = [
+  {
+    name: 'Doug',
+    graduated: false,
+    enrolled: true
+  },
+  {
+    name: 'Pat',
+    graduated: false,
+    enrolled: false
+  },
+  {
+    name: 'Marsha',
+    graduated: false,
+    enrolled: false
+  },
+  {
+    name: 'Moira',
+    graduated: false,
+    enrolled: true
+  },
+  {
+    name: 'Ben',
+    graduated: false,
+    enrolled: true
+  },
+  {
+    name: 'Nigel the Giraffe',
+    graduated: false,
+    enrolled: false
+  },
+  {
+    name: 'Brandon the Shark',
+    graduated: false,
+    enrolled: true
+  }
+];
 
 
+function graduateAndSetNewClass(array){
+	for (var i = 0; i < array.length; i++){
+		console.log (array[i]);
+		console.log(i);
+		if (array[i].enrolled === true){
+			console.log("enrolled equals true");
+			array[i].graduated = true;
+		} 
+		else if (array[i].enrolled === false){
+			array[i].enrolled = true;
+			console.log("enrolled equals false");
+		}	
+	}
+	return array;
+}
 
-
-
+console.log(graduateAndSetNewClass(currentClass));
 
 
 
